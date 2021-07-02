4617e707ee5594b5b1e6da1f576e50a4e3b18c61
Nicolás Naso <nnaso@santandertecnologia.com.ar>
2021-07-02 19:53:53 -0300

    feat(history): add term in the history collection

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ImageSearchService } from 'src/app/services/image-search.service';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
+  history$: Observable<string[]>;
 
-  constructor() { }
+  constructor(private readonly imageSearchService: ImageSearchService) {}
 
   ngOnInit() {
+    this.history$ = this.imageSearchService.getHistory();
   }
-
 }
--- src/app/services/image-search.service.ts
+++ src/app/services/image-search.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { Datum, GiphyResponse, Images } from '../models/giphy-response.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImageSearchService {
+  private history$: BehaviorSubject<string[]> = new BehaviorSubject([]);
+
   /**
    * Creates an instance of ImageSearchService.
    * @param {HttpClient} http
@@ -30,10 +32,26 @@ export class ImageSearchService {
     params = params.set('api_key', environment.giphy.apiKey);
     params = params.set('q', term);
 
+    this.addNewTerm(term);
+
     return this.http
       .get(url, { params })
       .pipe(map((response: GiphyResponse) => this.mapData(response)))
       .toPromise();
+  }
+
+  getHistory(): Observable<string[]> {
+    return this.history$.asObservable();
+  }
+
+  private addNewTerm(term: string) {
+    let terms: string[] = this.history$.value;
+    terms = [...terms, term];
+    this.history$.next(terms);
+  }
+
+  private getHistoryValues() {
+    return this.history$.value;
   }
 
   private mapData(response: GiphyResponse): Images[] {
